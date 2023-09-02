1941dd2c0904b5f171d97629ac262597d4551b3e
nagabob <kadiayalanaga83@gmail.com>
2023-09-02 13:51:12 +0530

    robo-shell

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,7 +4,8 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-06952f30fadb8a6b1
-
+DOMAIN_NAME=nagasweb.online
+HOSTED_ZONE_ID=Z03354052Q8Q1MDKAA6WP
 
 for i in "${NAMES[@]}"
 do
@@ -18,4 +19,17 @@ do
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 done
