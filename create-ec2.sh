48cccfc1fd81a564c39c5cf12f8246218ab51155
nagabob <kadiayalanaga83@gmail.com>
2023-09-02 13:33:26 +0530

    robo-shell

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-06952f30fadb8a6b1
 
 
 for i in "${NAMES[@]}"
-do 
-    echo "Name: $i"
+do
+    if [[$i == "mongodb"|| $i == "mysql"]]
+    then
+        INSTANCE_TYPE="t2.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
 done
