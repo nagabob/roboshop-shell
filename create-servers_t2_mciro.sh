65e82b604a7d1018e425df97300da9bfd7113386
nagabob <kadiayalanaga83@gmail.com>
2023-09-04 19:30:29 +0530

    shell

--- create-servers_t2_mciro.sh
+++ create-servers_t2_mciro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAMES=$@
-INSTANCE_TYPE=""
+INSTANCE_TYPE="t2.micro"
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-06952f30fadb8a6b1
 DOMAIN_NAME=nagasweb.online
@@ -9,13 +9,13 @@ HOSTED_ZONE_ID=Z03354052Q8Q1MDKAA6WP
 
 for i in $@
 do
-    if [[ $i == "mongodb" || $i == "mysql" ]]
+    # if [[ $i == "mongodb" || $i == "mysql" ]]
 
-    then
-        INSTANCE_TYPE="t3.medium"
-    else
-        INSTANCE_TYPE="t2.micro"
-    fi
+    # then
+    #     INSTANCE_TYPE="t3.medium"
+    # else
+    #     INSTANCE_TYPE="t2.micro"
+    # fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
@@ -23,7 +23,7 @@ do
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
-            "Action": "CREATE",
+            "Action": "UPSERT",
                         "ResourceRecordSet": {
                             "Name": "'$i.$DOMAIN_NAME'",
                             "Type": "A",
